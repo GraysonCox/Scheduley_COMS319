95beecb4942f63b08ea2a2c526b11c56dabf5484
Sebastian Kazun <smkazun@iastate.edu>
2018-11-09 11:12:28 -0600

    completed part of EmailSenderTest

---
+++ test/main/EmailSenderTest.java
@@ -0,0 +1,49 @@
+package main;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class EmailSenderTest {
+	
+	final String str1 = "We will not have any class today so please don't show up.";
+	final String str2 = "Please send me you order for Girl Scout Cookies as soon as you can. It is first come first serve and we only have a limited supply!";
+	
+	EmailSender email1 = new EmailSender("Class", str1);
+	EmailSender email2 = new EmailSender("GirlScoutCookies", str2);
+	
+	@Test
+	public void testSend() {
+		
+	}
+	
+	@Test
+	public void testSendAll() {
+		
+	}
+	
+	@Test
+	public void testGetSubject() {
+		assertEquals("Class", email1.getSubject());
+		assertEquals("GirlScoutCookies", email2.getSubject());
+		
+		email1.setSubject("Family");
+		assertEquals("Family", email1.getSubject());
+	}
+	
+	@Test
+	public void testGetBody() {
+		
+		assertEquals(str1, email1.getBody());
+		assertEquals(str2, email2.getBody());
+		
+		email1.setBody("The class is no longer cancelled.");
+		assertEquals("The class is no longer cancelled.", email1.getBody());
+		
+	}
+	
+	
+	
+	
+
+}
