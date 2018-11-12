b983aa73ac483f6002dd38f43eb836acaa715123
wasartin <wasartin@iastate.edu>
2018-11-12 03:42:33 +0000

    Update UserDAO.java

--- src/main/UserDAO.java
+++ src/main/UserDAO.java
@@ -9,13 +9,8 @@ public interface UserDAO {
 	
 	void insertUser(UserProfile newUser) throws SQLException;
 	void insertUser(String email, String name, UserType userType) throws SQLException;
-<<<<<<< HEAD
-	boolean updateUser(UserProfile user);
-=======
 	void updateUser(String email);
->>>>>>> adminMakeUserDBMethods
 	void deleteUser(String email) throws SQLException;
 	boolean verifyUser(String email, String password);
 	UserProfile findUser(String email);
 }
-
