32dc6533a46d99562823afe56325de51dce461fa
wasartin@iastate.edu <wasartin@iastate.edu>
2018-11-30 03:29:12 -0600

    seperated login into it's own method

--- src/main/ScheduleyApp.java
+++ src/main/ScheduleyApp.java
@@ -48,130 +48,88 @@ public class ScheduleyApp extends Application {
 
 	TranslateTransition openTree, closeTree, openSchedule, closeSchedule;
 
-	private boolean loginSuccessful;
+	private static boolean loginSuccessful;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("Scheduley");
-		GridPane grid = new GridPane();
-		grid.setAlignment(Pos.CENTER);
-		grid.setHgap(10);
-		grid.setVgap(10);
-		grid.setPadding(new Insets(25, 25, 25, 25));
-		Scene scene = new Scene(grid, 300, 275);
-		primaryStage.setScene(scene);
-		Text scenetitle = new Text("Welcome");
-		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-		grid.add(scenetitle, 0, 0, 2, 1);
-		Label userName = new Label("User Name:");
-		grid.add(userName, 0, 1);
-		TextField userTextField = new TextField();
-		grid.add(userTextField, 1, 1);
-		Label pw = new Label("Password:");
-		grid.add(pw, 0, 2);
-		PasswordField pwBox = new PasswordField();
-		grid.add(pwBox, 1, 2);
-		Button btn = new Button("Sign in");
-		HBox hbBtn = new HBox(10);
-		hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-		hbBtn.getChildren().add(btn);
-		grid.add(hbBtn, 1, 4);
-		final Text actionTarget = new Text();
-		grid.add(actionTarget, 1, 6);
-		pwBox.setOnKeyPressed(event -> {
-			if (event.getCode() == KeyCode.ENTER) {
-				btn.fire();
-			}
-		});
-		btn.setOnAction(e -> {
-			DAOFactory userDB = DAOFactory.getDAOFactory(DAOFactory.JDBC); //NEW Implementing the DAO properly
-			UserDAO dataSource = userDB.getUserDAO(); //NEW 
-			loginSuccessful = dataSource.verifyUser(userTextField.getText(), pwBox.getText()); //ORIGINAL
-			if (loginSuccessful) {
-				try {
-					VBox.setVgrow(basisRoot, Priority.ALWAYS);
+		loginScreen();
+		
+		try {
+			VBox.setVgrow(basisRoot, Priority.ALWAYS);
 
-					FXMLLoader menuBarLoader = new FXMLLoader(getClass().getResource("MenuBar.fxml"));
-					menuBar = menuBarLoader.load();
-					menuBarController = menuBarLoader.getController();
+			FXMLLoader menuBarLoader = new FXMLLoader(getClass().getResource("MenuBar.fxml"));
+			menuBar = menuBarLoader.load();
+			menuBarController = menuBarLoader.getController();
 
-					FXMLLoader basisLoader = new FXMLLoader(getClass().getResource("Basis.fxml"));
-					basis = basisLoader.load();
-					basisController = basisLoader.getController();
+			FXMLLoader basisLoader = new FXMLLoader(getClass().getResource("Basis.fxml"));
+			basis = basisLoader.load();
+			basisController = basisLoader.getController();
 
-					FXMLLoader treeLoader = new FXMLLoader(getClass().getResource("Tree.fxml"));
-					tree = treeLoader.load();
-					treeController = treeLoader.getController();
+			FXMLLoader treeLoader = new FXMLLoader(getClass().getResource("Tree.fxml"));
+			tree = treeLoader.load();
+			treeController = treeLoader.getController();
 
-					FXMLLoader scheduleLoader = new FXMLLoader(getClass().getResource("Schedule.fxml"));
-					schedule = scheduleLoader.load();
-					scheduleController = scheduleLoader.getController();
-					
-					FXMLLoader newMeetingFormLoader = new FXMLLoader(getClass().getResource("NewMeetingForm.fxml"));
-					newMeetingForm = newMeetingFormLoader.load();
-					newMeetingFormController = newMeetingFormLoader.getController();
-					
-					FXMLLoader manageUsersPaneLoader = new FXMLLoader(getClass().getResource("ManageUsersPane.fxml"));
-					manageUsersPane = manageUsersPaneLoader.load();
-					manageUsersPaneController = manageUsersPaneLoader.getController();
+			FXMLLoader scheduleLoader = new FXMLLoader(getClass().getResource("Schedule.fxml"));
+			schedule = scheduleLoader.load();
+			scheduleController = scheduleLoader.getController();
+			
+			FXMLLoader newMeetingFormLoader = new FXMLLoader(getClass().getResource("NewMeetingForm.fxml"));
+			newMeetingForm = newMeetingFormLoader.load();
+			newMeetingFormController = newMeetingFormLoader.getController();
+			
+			FXMLLoader manageUsersPaneLoader = new FXMLLoader(getClass().getResource("ManageUsersPane.fxml"));
+			manageUsersPane = manageUsersPaneLoader.load();
+			manageUsersPaneController = manageUsersPaneLoader.getController();
 
-					AnchorPane.setTopAnchor(basis, 0.0);
-					AnchorPane.setBottomAnchor(basis, 0.0);
-					AnchorPane.setLeftAnchor(basis, 30.0);
-					AnchorPane.setTopAnchor(tree, 0.0);
-					AnchorPane.setLeftAnchor(tree, -170.0);
-					AnchorPane.setBottomAnchor(tree, 0.0);
-					AnchorPane.setLeftAnchor(schedule, 30.0);
-					AnchorPane.setRightAnchor(schedule, 0.0);
-					AnchorPane.setBottomAnchor(schedule, -440.0);
-					AnchorPane.setTopAnchor(newMeetingForm, 0.0);
-					AnchorPane.setLeftAnchor(newMeetingForm, 30.0);
+			AnchorPane.setTopAnchor(basis, 0.0);
+			AnchorPane.setBottomAnchor(basis, 0.0);
+			AnchorPane.setLeftAnchor(basis, 30.0);
+			AnchorPane.setTopAnchor(tree, 0.0);
+			AnchorPane.setLeftAnchor(tree, -170.0);
+			AnchorPane.setBottomAnchor(tree, 0.0);
+			AnchorPane.setLeftAnchor(schedule, 30.0);
+			AnchorPane.setRightAnchor(schedule, 0.0);
+			AnchorPane.setBottomAnchor(schedule, -440.0);
+			AnchorPane.setTopAnchor(newMeetingForm, 0.0);
+			AnchorPane.setLeftAnchor(newMeetingForm, 30.0);
 
-					basisRoot.getChildren().addAll(basis, tree, schedule);
-					subRoot.getChildren().addAll(menuBar, basisRoot);
-					root.getChildren().addAll(subRoot, newMeetingForm, manageUsersPane);
+			basisRoot.getChildren().addAll(basis, tree, schedule);
+			subRoot.getChildren().addAll(menuBar, basisRoot);
+			root.getChildren().addAll(subRoot, newMeetingForm, manageUsersPane);
 
-					openTree = new TranslateTransition(new Duration(350), basisRoot);
-					openTree.setToX(0);
-					closeTree = new TranslateTransition(new Duration(350), basisRoot);
-					treeController.treeButton.setOnAction(event -> doTreeTransition());
+			openTree = new TranslateTransition(new Duration(350), basisRoot);
+			openTree.setToX(0);
+			closeTree = new TranslateTransition(new Duration(350), basisRoot);
+			treeController.treeButton.setOnAction(event -> doTreeTransition());
 
-					openSchedule = new TranslateTransition(new Duration(350), schedule);
-					openSchedule.setToY(0);
-					closeSchedule = new TranslateTransition(new Duration(350), schedule);
-					scheduleController.scheduleButton.setOnAction(event -> doScheduleTransition());
-					
-					menuBarController.newMeetingSpaceButton.setOnAction(event -> basisController.createMeetingSpace());
-					menuBarController.newFloorButton.setOnAction(event -> this.createFloor());
-					menuBarController.newMeetingButton.setOnAction(event -> newMeetingFormController.show());
-					menuBarController.manageUsersButton.setOnAction(event -> manageUsersPaneController.show());
+			openSchedule = new TranslateTransition(new Duration(350), schedule);
+			openSchedule.setToY(0);
+			closeSchedule = new TranslateTransition(new Duration(350), schedule);
+			scheduleController.scheduleButton.setOnAction(event -> doScheduleTransition());
+			
+			menuBarController.newMeetingSpaceButton.setOnAction(event -> basisController.createMeetingSpace());
+			menuBarController.newFloorButton.setOnAction(event -> this.createFloor());
+			menuBarController.newMeetingButton.setOnAction(event -> newMeetingFormController.show());
+			menuBarController.manageUsersButton.setOnAction(event -> manageUsersPaneController.show());
 
-					model = new DataModel(dataSource.findUser(userTextField.getText()));
-					menuBarController.initModel(model);
-					treeController.initModel(model);
-					basisController.initModel(model);
-					scheduleController.initModel(model);
-					newMeetingFormController.initModel(model);
-					manageUsersPaneController.initModel(model);
-					
+			menuBarController.initModel(model);
+			treeController.initModel(model);
+			basisController.initModel(model);
+			scheduleController.initModel(model);
+			newMeetingFormController.initModel(model);
+			manageUsersPaneController.initModel(model);
 
-					primaryStage.setTitle("Scheduley");
-					primaryStage.setScene(new Scene(root));
-					primaryStage.setMaximized(true);
-					primaryStage.show();
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-				primaryStage.setMaximized(true);
-			} else {
-				actionTarget.setFill(Color.FIREBRICK);
-				actionTarget.setText("Invalid login");
-			}
-		});
-
-		primaryStage.setScene(scene);
-		primaryStage.setMaximized(false);
-		primaryStage.show();
+			primaryStage.setTitle("Scheduley");
+			primaryStage.setScene(new Scene(root));
+			primaryStage.setMaximized(true);
+			primaryStage.show();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		primaryStage.setMaximized(true);
+		
 	}
 
 	@SuppressWarnings("unused")
@@ -210,6 +168,57 @@ public class ScheduleyApp extends Application {
 			closeSchedule.setToY(-400);
 			closeSchedule.play();
 		}
+	}
+	
+	public void loginScreen() {
+		Stage temp = new Stage();
+		temp.setTitle("Log in");
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25, 25, 25, 25));
+		Scene scene = new Scene(grid, 300, 275);
+		temp.setScene(scene);
+		Text scenetitle = new Text("Welcome");
+		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+		grid.add(scenetitle, 0, 0, 2, 1);
+		Label userName = new Label("User Name:");
+		grid.add(userName, 0, 1);
+		TextField userTextField = new TextField();
+		grid.add(userTextField, 1, 1);
+		Label pw = new Label("Password:");
+		grid.add(pw, 0, 2);
+		PasswordField pwBox = new PasswordField();
+		grid.add(pwBox, 1, 2);
+		Button btn = new Button("Sign in");
+		HBox hbBtn = new HBox(10);
+		hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+		hbBtn.getChildren().add(btn);
+		grid.add(hbBtn, 1, 4);
+		final Text actionTarget = new Text();
+		grid.add(actionTarget, 1, 6);
+		pwBox.setOnKeyPressed(event -> {
+			if (event.getCode() == KeyCode.ENTER) {
+				btn.fire();
+			}
+		});		
+		btn.setOnAction(e -> {
+			DAOFactory userDB = DAOFactory.getDAOFactory(DAOFactory.JDBC); 
+			UserDAO dataSource = userDB.getUserDAO();
+			loginSuccessful = dataSource.verifyUser(userTextField.getText(), pwBox.getText());
+			if(loginSuccessful){
+				model = new DataModel(dataSource.findUser(userTextField.getText()));
+				temp.close();
+			}else {
+				actionTarget.setFill(Color.FIREBRICK);
+				actionTarget.setText("Invalid login");
+			}
+		});
+
+		temp.setMaximized(false);
+		temp.showAndWait();
+
 	}
 
 	public static void main(String[] args) {
